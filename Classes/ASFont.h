b82abc8374bce71c6bb79938842081e0153b7f2e
Jason L Perry <jasper@ambethia.com>
2010-02-19 17:21:05 -0500

    Fix file locations

---
+++ Classes/ASFont.h
@@ -0,0 +1,16 @@
+//
+//  ASFont.h
+//  Aposelene
+//
+//  Created by Jason Perry on 2/19/10.
+//  Copyright 2010 Ambethia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ASFont : NSObject {
+
+}
+
+@end
---
+++ Classes/ASFont.m
@@ -0,0 +1,14 @@
+//
+//  ASFont.m
+//  Aposelene
+//
+//  Created by Jason Perry on 2/19/10.
+//  Copyright 2010 Ambethia. All rights reserved.
+//
+
+#import "ASFont.h"
+
+
+@implementation ASFont
+
+@end
